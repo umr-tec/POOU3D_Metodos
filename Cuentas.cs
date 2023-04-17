e496c4c72852f7f02d78d7a24f3e36771ecb7f29
Ulises MR <umr.user@gmail.com>
2023-04-17 15:14:13 -0600

    v1 Métodos de tipo Primitivo

---
+++ Cuentas.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace POOU3D_Ejemplo1
+{
+    class Cuentas
+    {
+        //Campos - banco - cuenta - persona
+        string nombreCliente, primerApellidoCliente, segundoApellidoCliente;
+        string curpCliente, rfcCLiente, numeroCelularCliente, emailCliente;
+        string numeroCuenta;
+        DateTime fechaRegistroCliente;
+        double saldoActual, saldoAnterior;
+        const string nombreBanco = "ChucanflurBank";
+
+        public Cuentas(string nombreCliente, string primerApellidoCliente, string segundoApellidoCliente, string rfcCLiente, string numeroCelularCliente, string emailCliente)
+        {
+            this.nombreCliente = nombreCliente;
+            this.primerApellidoCliente = primerApellidoCliente;
+            this.segundoApellidoCliente = segundoApellidoCliente;
+            this.rfcCLiente = rfcCLiente;
+            this.numeroCelularCliente = numeroCelularCliente;
+            this.emailCliente = emailCliente;
+        }
+
+        public Cuentas()
+        {
+            Console.Title = nombreBanco;
+        }
+
+        //Metodo para crear o eliminar una cuenta 
+        public bool AgregarEliminarCuenta() {
+            //Variable de pulsacion de teclado
+            ConsoleKeyInfo seleccionTecla;
+            Console.WriteLine("Adminiatración de cuentas.");
+
+            if (string.IsNullOrEmpty(rfcCLiente))//""
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error: Debes ingresar un RFC para dar de alta una cuenta.");
+            }
+            else
+            {
+                Console.WriteLine("Se va a agregar o eliminar una cuenta para el cliente con RFC: {0}.", rfcCLiente);
+                Console.WriteLine("\n Selecciona la opción que desees ejecutar:");
+                Console.WriteLine("\t Presiona 1: Si deseas agregar una cuenta al cliente");
+                Console.WriteLine("\t Presiona 2: Si deseas Eliminar la cuenta al cliente");
+                seleccionTecla = Console.ReadKey();
+
+                if (seleccionTecla.Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine("Presionastye el número 1");
+                }
+                if (seleccionTecla.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine("Presionastye el número 2");
+                }
+            }                           
+            return false;
+        }
+
+
+        //Miembro de tipo metodo
+        public int CalcularCosto( int cantidadArticulos, int costoArticulos ) {
+            return cantidadArticulos*costoArticulos;
+        }
+        public string CalcularCosto2(string nombreArticulo, int cantidadArticulos, int costoArticulos) {
+            int resultado = cantidadArticulos * costoArticulos;
+            return nombreArticulo +" Total: $"+ resultado.ToString();
+        }
+        public void CalcularCosto1() {
+            Console.WriteLine("0");
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace POOU3D_Ejemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear la instancia
+            Cuentas objeto1 = new Cuentas();
+            //Mandar llamar los métodos
+            Console.WriteLine(objeto1.CalcularCosto2("Marcador TOP", 9, 10));
+            Console.WriteLine( objeto1.CalcularCosto(2,900));
+            objeto1.CalcularCosto1();
+
+            objeto1.AgregarEliminarCuenta();            
+            /*
+                +
+                - Nahomi
+             */
+            Console.ReadKey();
+        }
+    }
+}
